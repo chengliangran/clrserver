7164fdbd4c421cef3d7d88177e687da613f2fa97
程亮然 <1519790385@qq.com>
2017-03-08 18:02:22 +0800

    c

--- web-test/finalserver/src/com/finalserver/core/container/Container.java
+++ web-test/finalserver/src/com/finalserver/core/container/Container.java
@@ -1,8 +1,36 @@
 package com.finalserver.core.container;
 
+import com.finalserver.core.container.components.Loader;
+import com.finalserver.core.container.components.Logger;
+import com.finalserver.core.container.components.Mapper;
+import com.finalserver.core.container.components.PipeLine;
+
 /**
  * Created by Administrator on 2017-03-06.
  */
-public interface Container {
+public interface Container{ 
+
+    public Logger getLogger(); 
+    
+
+    public void setLogger(Logger logger);
+
+    public Loader getLoader(); 
+    
+
+    public void setLoader(Loader loader); 
+    
+
+    public Mapper getMapper();
+    
+
+    public void setMapper(Mapper mapper); 
+    
+
+    public PipeLine getPipeLine();
+    
+
+    public void setPipeLine(PipeLine pipeLine);
+
 
 }
--- web-test/finalserver/src/com/jfinalserver/test/Test.java
+++ web-test/finalserver/src/com/jfinalserver/test/Test.java
@@ -3,21 +3,29 @@ package com.jfinalserver.test;
 import com.finalserver.core.Constants;
 
 import java.io.*;
+import java.security.Timestamp;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.SimpleFormatter;
 
 /**
  * Created by Administrator on 2017-03-07.
  */
 public class Test {
     public static void main(String[] args) {
-        File file=new File(Constants.WEB_ROOT,"a.txt");
-        try {
-            FileInputStream fileInputStream=new FileInputStream(file);
-            byte[] buf=new byte[4];
-            fileInputStream.read(buf);
-            System.out.println(new String(buf));
-          } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Date date=new Date();
+        System.out.println(date);
+        String format="yyyymmdd HHMMSS";
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat(format);
+        String dates= simpleDateFormat.format(date);
+        System.out.println(dates);
+        java.sql.Timestamp timestamp=new java.sql.Timestamp(System.currentTimeMillis());
+        System.out.println(timestamp);
+
+
+
+
 
 
     }
